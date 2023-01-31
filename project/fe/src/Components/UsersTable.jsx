3772f8be1d11ed7b6ba2fdd0fb8d347cc50d84ac
Morning <morning@admins-iMac-3.local>
2023-01-31 12:41:29 +0800

    few tweaks

--- project/fe/src/Components/UsersTable.jsx
+++ project/fe/src/Components/UsersTable.jsx
@@ -2,30 +2,44 @@ import { DataGrid } from "@mui/x-data-grid";
 
 export default function UsersTable() {
   const columns = [
-    { field: "id", headerName: "ID", width: 70 },
-    { field: "firstName", headerName: "First name", width: 130 },
-    { field: "lastName", headerName: "Last name", width: 130 },
-    {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      width: 90,
-    },
-    {
-      field: "fullName",
-      headerName: "Full name",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-    },
+    { field: "firstName", headerName: "First name", width: 260 },
+    { field: "lastName", headerName: "Last name", width: 260 },
+    { field: "PhoneNum", headerName: "Phone Number", width: 260 },
+    { field: "email", headerName: "E-Mail", width: 260 },
+    { field: "Role", headerName: "Role", width: 260 },
+    { field: "Disabled", headerName: "Disabled", width: 260 },
+    { field: "avatar", headerName: "Avatar", width: 260 },
+    { field: "action", headerName: "Actions", width: 260 },
   ];
   const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    {
+      id: 1,
+      lastName: "Snow",
+      firstName: "Jon",
+      email: "john@doe.com",
+      role: "user",
+    },
+    {
+      id: 2,
+      lastName: "Lannister",
+      firstName: "Cersei",
+      email: "john@doe.com",
+      role: "user",
+    },
+    {
+      id: 3,
+      lastName: "Lannister",
+      firstName: "Jaime",
+      email: "john@doe.com",
+      role: "user",
+    },
+    {
+      id: 4,
+      lastName: "Stark",
+      firstName: "Arya",
+      email: "john@doe.com",
+      role: "admin",
+    },
     { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
     { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
     { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
